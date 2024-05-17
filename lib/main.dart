7250b7bdcef4523055554d18aeecacaf673fdb46
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-05-17 23:16:45 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class TeksUtama extends StatefulWidget {
+  final state = StateTeksUtama();
+
+  TeksUtama({super.key});
+
+  @override
+  State<StatefulWidget> createState() => state;
+}
+
+class StateTeksUtama extends State<TeksUtama> {
+  var listNama = [
+    'Muhammad Rifki',
+    'Reza Yuli Santosa',
+    'Virda Romadani',
+    'Septi Aprilia Wulandari',
+    'Fatin Nurmalaningrum',
+  ];
+  var listWarna = [
+    Colors.red,
+    Colors.brown,
+    Colors.green,
+    Colors.pink,
+    Colors.blue,
+  ];
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'Apa kabar',
+          textDirection: TextDirection.ltr,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Text(
+          textAlign: TextAlign.center,
+          listNama[index % listNama.length],
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+            color: listWarna[index % listWarna.length],
+          ),
+        )
+      ],
+    );
+  }
+
+  void incrementIndex() {
+    setState(() {
+      index++;
+    });
+  }
+}
+
+void main() {
+  TeksUtama teksUtama = TeksUtama();
+  runApp(MaterialApp(
+      title: 'Halo Dunia',
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue[800],
+          title: const Text(
+            'Halo Dunia',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(child: teksUtama),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Random',
+          onPressed: teksUtama.state.incrementIndex,
+          child: const Icon(Icons.refresh),
+        ),
+      )));
+}
